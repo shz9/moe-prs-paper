24e4e7730cd6259999bc97dff6fd3ba6d2ce15e5
Shadi <shadizabad1@gmail.com>
2026-03-25 14:09:37 +0000

    Updating VIPRS inference scripts

--- data_preparation/1_prepare_pgs_weights/prepare_sex_biased_pgs/train_viprs.sh
+++ data_preparation/1_prepare_pgs_weights/prepare_sex_biased_pgs/train_viprs.sh
@@ -15,14 +15,14 @@ do
     viprs_fit -l "data/ld/EUR/chr_*/" \
               -s "data/external_sumstats/sex_stratified/${phenotype}/${stratum}.glm.linear" \
               --sumstats-format "plink2" \
-              --output-dir "data/pgs_weights/${phenotype}/" \
-              --output-file-prefix "${stratum}_"
+              --output-dir "data/pgs_weights/" \
+              --output-file-prefix "${phenotype}_${stratum}"
     python3 data_preparation/1_prepare_pgs_weights/utils/harmonize_inferred_beta.py \
-            --input-file "data/pgs_weights/${phenotype}/${stratum}_VIPRS_EM.fit.gz" \
+            --input-file "data/pgs_weights/${phenotype}_${stratum}_VIPRS_EM.fit.gz" \
             --pgs-name "${phenotype}_${stratum}" \
             --lift-over
-    rm -rf "data/pgs_weights/${phenotype}/${stratum}_VIPRS_EM.fit.gz"
+    rm -rf "data/pgs_weights/${phenotype}_${stratum}_VIPRS_EM.fit.gz"
   done
   # Remove hyperparameter files:
-  rm -rf "data/pgs_weights/${phenotype}/"*.hyp
+  rm -rf "data/pgs_weights/${phenotype}_${stratum}"*.hyp
 done
